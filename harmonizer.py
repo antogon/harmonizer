bff763c9c0ad81099cc1bcca5c985ab82b00cdc2
Antonio MalvaGomes <antonio@distru.com>
2024-02-03 12:42:04 -0600

    Parses rudimentary tablature into notes

--- harmonizer.py
+++ harmonizer.py
@@ -1,18 +1,38 @@
 import argparse
-from music21 import chord, converter, stream
+import re
+from music21 import chord, converter, stream, scale, pitch
+
+def parse_tablature(tablature):
+    valid_tablature_regex = r'[\n\w]*([a-gA-G#]{1,2}(\|[-0-9]+)+\|?\w*\n?){4,6}'
+    if re.match(valid_tablature_regex, tablature):
+        one_line_regex = r'([a-gA-G#](\|[-0-9]+)+\|?\n)'
+        parsed_notes = []
+        for line_match in re.finditer(one_line_regex, tablature):
+            line_breakup_match = re.match(r'[\w\n]*([a-gA-G#]{1,2})((\|[-0-9]+)+)\|?', line_match.group(0))
+            starting_pitch = line_breakup_match.group(1)
+            chromatic_scale = scale.ChromaticScale('g2')
+            for note_match in re.finditer(r'([0-9]+)', line_breakup_match.group(2)):
+                match_index = note_match.start()
+                note_as_semitone_count = int(note_match.group(1))
+                origin_pitch = pitch.Pitch(starting_pitch)
+                adjusted_pitch = chromatic_scale.nextPitch(origin_pitch, note_as_semitone_count)
+                parsed_notes.append((match_index, adjusted_pitch))
+        return list(map(lambda note_tuple: note_tuple[1], sorted(parsed_notes, key=lambda note_tuple: note_tuple[0])))
+    else:
+        raise RuntimeError("Invalid tablature")
 
 def harmonize(tinynotation_notes, number_of_steps):
-    scale = converter.parse(tinynotation_notes)
-    key = scale.analyze('key')
+    input_line = converter.parse(tinynotation_notes)
+    key = input_line.analyze('key')
 
     harmony_pitches = []
 
-    for pitch in scale.pitches:
-        harmony_pitches.append(key.nextPitch(pitch, stepSize=number_of_steps))
+    for input_pitch in input_line.pitches:
+        harmony_pitches.append(key.nextPitch(input_pitch, stepSize=number_of_steps))
 
     harmonized_line = []
 
-    for given_pitch, harmony_pitch in list(zip(scale.pitches, harmony_pitches)):
+    for given_pitch, harmony_pitch in list(zip(input_line.pitches, harmony_pitches)):
         c = chord.Chord([given_pitch, harmony_pitch])
         harmonized_line.append(c)
 
